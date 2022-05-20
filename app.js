76f483a26d369c1acefc458d39d421c608d510e2
Elvis Boves <elvisboves@Elviss-MacBook-Pro.local>
2022-05-19 20:58:54 -0400

    type: style
     body: Finish all the layout. Working on the blogs.
    
     footer: #22

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+//                      MOBILE NAVEGATION                            //
+//                                                                   //
+//////////////////////////////////////////////////////////////////////
+
 const btn_nav_el = document.querySelector(".btn-nav");
 const header_el = document.querySelector(".header");
 
 btn_nav_el.addEventListener("click", function () {
   header_el.classList.toggle("nav-open");
 });
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+//                        BLOG HOME PAGE                             //
+//                                                                   //
+//////////////////////////////////////////////////////////////////////
+
+// Read me
+const post1 = document.querySelector(".post1");
+const post2 = document.querySelector(".post2");
+const post3 = document.querySelector(".post3");
+
+post1.addEventListener("click", function () {
+  window.open("blog_post.html");
+});
+
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+//                         BLOG POST PAGE                            //
+//                                                                   //
+//////////////////////////////////////////////////////////////////////
